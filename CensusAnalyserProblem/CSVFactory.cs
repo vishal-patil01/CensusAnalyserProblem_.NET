0775654d7469274f24c28a576dc6233807e3a9f0
vishal-patil01 <vishalpatil01@hotmail.com>
2020-08-10 11:02:06 +0530

    Refactored :Create Separate CSVReaderException To Encapsulate Changes

--- CensusAnalyserProblem/CSVFactory.cs
+++ CensusAnalyserProblem/CSVFactory.cs
@@ -1,4 +1,5 @@
-﻿namespace CensusAnalyserProblem
+﻿
+namespace CensusAnalyserProblem
 {
     class CSVFactory
     {
--- CensusAnalyserProblem/ICSVReader.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace CensusAnalyserProblem
-{
-    interface ICSVReader
-    {
-        public delegate string[] ReadCSVFileData(string headers, string csvFilePath);
-        public string[] getCSVFileData(string headers, string csvFilePath);
-    }
-}
